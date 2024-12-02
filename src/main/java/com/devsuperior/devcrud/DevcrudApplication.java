780ea751d90f8a0bfd9b9f08e6241041093e5239
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-12-02 06:46:41 -0300

    Project started, H2 database configuration and JPA persistence

---
+++ src/main/java/com/devsuperior/devcrud/DevcrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/devcrud/DevcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
